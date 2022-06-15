a866e5e5cf742d6b32d53b4b736dbf1078e1970a
Re:Alise <28112294+Re-Alise@users.noreply.github.com>
2022-06-15 19:39:58 +0900

    Add basic menu
    Menu scripts have not implemented yet, just clean up some code.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron')
 const path = require('path')
 
+let autoConnect
+
+function saveAsFile() {
+    console.info('saveAsFile triggered')
+}
+
+function openFile() {
+    console.info('openFile triggered')
+}
+
+function openFileConnect() {
+    console.info('openFileConnect triggered')
+}
+
+function openOriginalFile() {
+    console.info('openOriginalFile triggered')
+}
+
+function qlabCue() {
+    console.info('qlabCue triggered')
+}
+
+function listSceneItems() {
+    console.info('listSceneItems triggered')
+}
+
 function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
@@ -13,49 +39,143 @@ function createWindow() {
         }
     })
 
-    const menu = Menu.buildFromTemplate([
-        {
-            label: app.name,
+    const isMac = process.platform === 'darwin'
+    const menuTemplate = [
+        ...(isMac ? [{
+            label: "OBSosc",
             submenu: [
+                { role: 'about' },
+                { type: 'separator' },
+                { role: 'services' },
+                { type: 'separator' },
+                { role: 'hide' },
+                { role: 'hideothers' },
+                { role: 'unhide' },
+                { type: 'separator' },
+                { role: 'quit' }
+            ]
+        }] : []),
+        {
+            label: 'File',
+            submenu: [
+                isMac ? { role: 'close' } : { role: 'quit' },
                 {
-                    label: 'Increment',
-                    click: () => mainWindow.webContents.send('update-counter', 1)
+                    label: 'Save As...',
+                    accelerator: 'Shift+CommandOrControl+s',
+                    click: () => saveAsFile()
                 },
                 {
-                    label: 'Decrement',
-                    click: () => mainWindow.webContents.send('update-counter', -1)
+                    label: 'Open',
+                    accelerator: 'CommandOrControl+o',
+                    click: () => openFile()
+
+                },
+                {
+                    label: 'Open/Connect',
+                    accelerator: 'CommandOrControl+Shift+o',
+                    click: () => openFileConnect()
+
+                },
+                {
+                    label: 'Automatically Connect on Startup',
+                    type: 'checkbox',
+                    checked: false,
+                    click: (item) => {
+                        if (item.checked == false) {
+                            autoConnect = false
+                        } else if (item.checked == true) {
+                            autoConnect = true
+                        }
+                    }
+                },
+                {
+                    label: 'Revert to Default Values',
+                    accelerator: 'CommandOrControl+Shift+/',
+                    click: () => openOriginalFile()
                 }
             ]
-        }
-    ])
+        },
+        {
+            label: 'Edit',
+            submenu: [
+                { role: 'undo' },
+                { role: 'redo' },
+                { type: 'separator' },
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                ...(isMac ? [
+                    { role: 'pasteAndMatchStyle' },
+                    { role: 'delete' },
+                    { role: 'selectAll' },
+                    { type: 'separator' },
+                    {
+                        label: 'Speech',
+                        submenu: [
+                            { role: 'startSpeaking' },
+                            { role: 'stopSpeaking' }
+                        ]
+                    }
+                ] : [
+                    { role: 'delete' },
+                    { type: 'separator' },
+                    { role: 'selectAll' }
+                ])
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                { role: 'reload' },
+                { role: 'forceReload' },
+                { role: 'toggleDevTools' },
+                { type: 'separator' },
+                { role: 'resetZoom' },
+                { role: 'zoomIn' },
+                { role: 'zoomOut' },
+                { type: 'separator' },
+                { role: 'togglefullscreen' }
+            ]
+        },
+        {
+            label: 'Window',
+            submenu: [
+                { role: 'minimize' },
+                { role: 'zoom' },
+                ...(isMac ? [
+                    { type: 'separator' },
+                    { role: 'front' },
+                    { type: 'separator' },
+                    { role: 'window' }
+                ] : [
+                    { role: 'close' }
+                ])
+            ]
+        },
+        {
+            label: 'Scripts',
+            submenu: [
+                {
+                    label: 'Populate QLab OSC Cues From OBS Scenes',
+                    accelerator: 'CommandOrControl+1',
+                    click: () => qlabCue()
+                },
+                {
+                    label: 'Log All Available Scene Items (Sources)',
+                    accelerator: 'CommandOrControl+2',
+                    click: () => listSceneItems()
+                },
+            ]
+        },
+    ]
 
+    const menu = Menu.buildFromTemplate(menuTemplate)
     Menu.setApplicationMenu(menu)
-    ipcMain.on('set-title', changeTitle)
-
     mainWindow.loadFile(path.join(__dirname, 'index.html'))
     mainWindow.webContents.openDevTools()
 }
 
-function changeTitle(event, title) {
-    const webContent = event.sender
-    const win = BrowserWindow.fromWebContents(webContent)
-    win.setTitle(title)
-}
-
-async function openFile() {
-    const { canceled, filePaths } = await dialog.showOpenDialog()
-    if (canceled) {
-        return
-    }
-
-    return filePaths[0]
-}
-
 app.whenReady().then(() => {
-    ipcMain.handle('dialog:openFile', openFile)
-    ipcMain.on('counter-value', (_event, value) => {
-        console.info(`Value changed: ${value}`)
-    })
     createWindow()
 
     app.on('activate', () => {
